770b3cd22d6a8f575e98b7bc1948a1587ca1f463
Jason Summer <jasummer92@gmail.com>
2025-01-08 09:12:44 -0600

    Add documentation

--- src/gmail/__init__.py
+++ src/gmail/__init__.py
@@ -6,11 +6,11 @@ def main():
     """Main entry point for the package."""
     parser = argparse.ArgumentParser(description='Gmail API MCP Server')
     parser.add_argument('--creds-file-path',
-                        # required=True,
-                       help='blah blah blah')
-    parser.add_argument('--target-token-path',
-                        # required=True,
-                       help='blah blah blah')
+                        required=True,
+                       help='OAuth 2.0 credentials file path')
+    parser.add_argument('--token-path',
+                        required=True,
+                       help='File location to store and retrieve access and refresh tokens for application')
     
     args = parser.parse_args()
     asyncio.run(server.main(args.creds_file_path, args.target_token_path))
